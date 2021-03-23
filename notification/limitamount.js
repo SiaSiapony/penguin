ac08ff2f60cbaade600a5a5ea01563b81153ea14
minso sogood <min_so123@hotmail.com>
2021-03-23 18:25:37 +0700

    มิ้นนน
    วัดดดด

---
+++ notification/limitamount.js
@@ -0,0 +1,12 @@
+
+function test(){
+    let nameingredient = document.getElementById('name').value
+    let a = document.getElementById('amount').value
+    let u = document.getElementsById('unit').value
+    if(a > 10){
+        alert("ไอ้สัสสสสสสสสส! กรอกจำนวนเกินที่กำหนด")
+    }
+    console.log(nameingredient,a,u)
+ 
+}
+
